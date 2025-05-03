4faa219200ebff1176732d404928e83d18d1e3b4
Hoursesoft <martinezmunozbreyners@gmail.com>
2025-05-03 01:46:44 +0000

    Ya se pueden hacer correctamente peticiones

--- src/database/db.js
+++ src/database/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-    db: "mongodb+srv://myadmin:<bd3138619952>@cluster0.8ggs6q5.mongodb.net"
+    db: "mongodb+srv://ciclo3:ciclo3@cluster0.a6x9d.mongodb.net/utp_grupo2?retryWrites=true&w=majority"
     //db: "mongodb://localhost:27017/utp_grupo2"
 }
