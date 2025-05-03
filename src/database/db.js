97cf18fe988bb9b0cb5582df147e0a413b9f7e9a
Hoursesoft <martinezmunozbreyners@gmail.com>
2025-05-03 01:38:53 +0000

    Se hizo correctamente la conexion a la bd

--- src/database/db.js
+++ src/database/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-    db: "mongodb+srv://myadmin:<bd3138619952>@cluster0.8ggs6q5.mongodb.net/"
+    db: "mongodb+srv://myadmin:<bd3138619952>@cluster0.8ggs6q5.mongodb.net"
     //db: "mongodb://localhost:27017/utp_grupo2"
 }
